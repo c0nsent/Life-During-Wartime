f9f6548a2c37d6f577efbc303e76b886bf06d81e
consent <c0nsent@proton.me>
2026-02-11 19:32:44 +0300

    commit

--- 01_instance.cc
+++ 01_instance.cc
@@ -4,8 +4,9 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-#include <ostream>
-
+#include <ranges>
+#include <cstdint>
+#include <stdexcept>
 
 
 using i32 = std::int32_t;
@@ -19,6 +20,36 @@ class HelloTriangleApplication
 {
 	void createInstance()
 	{
+		constexpr vk::ApplicationInfo appInfo{
+			.pApplicationName = "Hello Triangle",
+			.applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+			.pEngineName = "Life During Wartime",
+			.engineVersion = VK_MAKE_VERSION(1, 0, 0),
+			.apiVersion = vk::ApiVersion14
+		};
+
+		u32 glfwExtensionCount{};
+		const auto glfwExtensions{ glfwGetRequiredInstanceExtensions(&glfwExtensionCount) };
+
+		const auto extensionProperties{ context.enumerateInstanceExtensionProperties() };
+
+		for (u32 i{0}; i < glfwExtensionCount; ++i)
+		{
+			if (std::ranges::none_of(extensionProperties,
+				[glfwExtension{glfwExtensions[i]}](auto const& extensionProperty)
+				{
+					return std::strcmp(extensionProperty.extensionName, glfwExtension) == 0;
+				}))
+				throw std::runtime_error{"Required GLFW extension not supported: " + std::string(glfwExtensions[i])};
+		}
+
+		const vk::InstanceCreateInfo createInfo {
+			.pApplicationInfo = &appInfo,
+			.enabledExtensionCount = glfwExtensionCount,
+			.ppEnabledExtensionNames = glfwExtensions
+		};
+
+		instance = vk::raii::Instance(context, createInfo);
 
 	}
 
@@ -69,12 +100,19 @@ private:
 	GLFWwindow* window;
 
 	vk::raii::Context context;
-	vk::raii::Instance instance;
+	vk::raii::Instance instance{nullptr}; //Дефолтный конструктор удален
 };
 
 int main()
 {
-	HelloTriangleApplication app;
+	try
+	{
+		HelloTriangleApplication app;
+		app.run(WIDTH, HEIGHT);
+	}
+	catch (const std::runtime_error& e)
+	{
+		throw std::runtime_error{std::string{e.what()}};
+	}
 
-	app.run(WIDTH, HEIGHT);
 }
